539652a97919bcd956aa0279ff6e1f7c20801b6e
Christian Doczkal <20443222+chdoc@users.noreply.github.com>
2025-03-01 16:14:37 +0100

    more fields and execution timing

--- plugins/export-map.cpp
+++ plugins/export-map.cpp
@@ -11,12 +11,14 @@
 #include "df/world_data.h"
 #include "df/region_map_entry.h"
 #include "df/world_region.h"
+#include "df/world_landmass.h"
 #include "df/world_region_details.h"
 
 #include "gdal/ogrsf_frmts.h"
 
 #include <string>
 #include <vector>
+#include <chrono>
 
 using std::string;
 using std::vector;
@@ -47,7 +49,7 @@ DFhackCExport command_result plugin_init(color_ostream &out, std::vector <Plugin
 auto setGeometry(OGRFeature *feature, double x, double y, double dim) {
     auto poly = new OGRPolygon();
     auto boundary = new OGRLinearRing();
-    y = -y;
+    y = -y; // in GIS negative y-coordinates mean further south
     boundary->addPoint(x,y);
     boundary->addPoint(x,y-dim);
     boundary->addPoint(x+dim,y-dim);
@@ -92,8 +94,35 @@ auto create_field(OGRLayer *layer, std::string name, OGRFieldType type, int widt
 // PROJ.4 description of EPSG:3857 (https://epsg.io/3857)
 static const char* EPSG_3857 = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs";
 
+
+
+const char* describe_surroundings(int savagery, int evilness) {
+    constexpr std::array<const char*,9>surroundings{
+        "Serene",   "Mirthful",     "Joyous Wilds",
+        "Calm",     "Wilderness",   "Untamed Wilds",
+        "Sinister", "Haunted",      "Terrifying"
+    };
+    auto savagery_index = savagery < 33 ? 0 : (savagery > 65 ? 2 : 1);
+    auto evilness_index = evilness < 33 ? 0 : (evilness > 65 ? 2 : 1);
+    return surroundings[3 * evilness_index + savagery_index];
+}
+
 static command_result do_command(color_ostream &out, vector<string> &parameters) {
+    // we don't want DF to delete region details while we are iterating over them...
+    CoreSuspender suspend;
+
+    if (!Core::getInstance().isWorldLoaded()){
+        out.printerr("This command requires a world to be loaded\n");
+        return CR_WRONG_USAGE;
+    }
+
+    out.print("%lu out of %d region map tiles loaded\n",
+        world->world_data->midmap_data.region_details.size(),
+        world->world_data->world_width * world->world_data->world_height
+    );
     out.print("exporting map... ");
+    out.flush();
+    const auto start{std::chrono::steady_clock::now()};
 
     // set up coordinate system
     OGRSpatialReference CRS;
@@ -120,16 +149,30 @@ static command_result do_command(color_ostream &out, vector<string> &parameters)
     auto layer = dataset->CreateLayer( "world_biomes", &CRS, wkbPolygon, NULL );
 
     try {
-        create_field(layer, "biome_type", OFTString, 32);
-        create_field(layer, "elevation", OFTInteger);
-        create_field(layer, "evilness", OFTInteger);
-        create_field(layer, "savagery", OFTInteger);
         create_field(layer, "region_id", OFTInteger);
         create_field(layer, "region_name_en", OFTString, 100);
         create_field(layer, "region_name_df", OFTString, 100);
         create_field(layer, "landmass_id", OFTInteger);
+        create_field(layer, "landmass_name_en", OFTString, 100);
+        create_field(layer, "landmass_name_df", OFTString, 100);
+
+        create_field(layer, "biome_type", OFTString, 32);
+        create_field(layer, "surroundings", OFTString, 16);
+        create_field(layer, "elevation", OFTInteger);
+
+        create_field(layer, "evilness", OFTInteger);
+        create_field(layer, "savagery", OFTInteger);
         create_field(layer, "volcanism", OFTInteger);
         create_field(layer, "drainage", OFTInteger);
+        create_field(layer, "temperature", OFTInteger);
+        create_field(layer, "vegetation", OFTInteger);
+        create_field(layer, "rainfall", OFTInteger);
+        create_field(layer, "snowfall", OFTInteger);
+        create_field(layer, "salinity", OFTInteger);
+
+        create_field(layer, "reanimating", OFTInteger, OFSTBoolean);
+        create_field(layer, "has_bogeymen", OFTInteger, OFSTBoolean);
+
     }catch (const DFHack::command_result& r) {
         out.printerr("could not create fields for output layer");
         return r;
@@ -179,13 +222,20 @@ static command_result do_command(color_ostream &out, vector<string> &parameters)
                 // gets supplementary information from the world tile
                 auto& world_entry = world->world_data->region_map[biome_x][biome_y];
                 #define SET_FIELD(name) feature->SetField( #name, world_entry.name)
-                SET_FIELD(evilness);
-                SET_FIELD(savagery);
                 SET_FIELD(region_id);
                 SET_FIELD(landmass_id);
+                SET_FIELD(evilness);
+                SET_FIELD(savagery);
                 SET_FIELD(volcanism);
                 SET_FIELD(drainage);
+                SET_FIELD(temperature);
+                SET_FIELD(vegetation);
+                SET_FIELD(rainfall);
+                SET_FIELD(snowfall);
+                SET_FIELD(salinity);
                 #undef SET_FIELD
+
+                feature->SetField( "surroundings", describe_surroundings(world_entry.savagery, world_entry.evilness));
 
                 auto region = df::world_region::find(world_entry.region_id);
                 if (region) {
@@ -193,6 +243,15 @@ static command_result do_command(color_ostream &out, vector<string> &parameters)
                     feature->SetField( "region_name_en", region_name_en.c_str());
                     auto region_name_df = DF2UTF(Translation::translateName(&region->name, false));
                     feature->SetField( "region_name_df", region_name_df.c_str());
+                    feature->SetField("reanimating", region->reanimating);
+                    feature->SetField("has_bogeymen", region->has_bogeymen);
+                }
+                auto landmass = df::world_landmass::find(world_entry.landmass_id);
+                if (landmass) {
+                    auto landmass_name_en = DF2UTF(Translation::translateName(&landmass->name, true));
+                    feature->SetField( "landmass_name_en", landmass_name_en.c_str());
+                    auto landmass_name_df = DF2UTF(Translation::translateName(&landmass->name, false));
+                    feature->SetField( "landmass_name_df", landmass_name_df.c_str());
                 }
 
                 // this updates the feature with the id it receives in the layer
@@ -206,6 +265,8 @@ static command_result do_command(color_ostream &out, vector<string> &parameters)
     #endif
 
     GDALClose( dataset );
-    out.print("done!\n");
+    const auto finish{std::chrono::steady_clock::now()};
+    const std::chrono::duration<double> elapsed_seconds{finish - start};
+    out.print("done in %f ms !\n", elapsed_seconds.count());
     return CR_OK;
 }
